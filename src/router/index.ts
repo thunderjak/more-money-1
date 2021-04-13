014cd1d45eef7bd8d0bea7c4ac8b5aca6206c771
thunderjak <1026253290@qq.com>
2021-04-13 18:54:29 +0800

    添加404 页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import Home from '@/views/Home.vue'
 import Money from '@/views/money.vue'
 import Labels from '@/views/labels.vue'
 import Statistics from '@/views/statistics.vue'
+import NotFound from '@/views/NotFound.vue'
 
 Vue.use(VueRouter)
 
@@ -23,6 +24,10 @@ const routes: Array<RouteConfig> = [
   {
     path: '/statistics',
     component: Statistics
+  },
+  {
+    path:'*', //路由从上到下的顺序进行匹配
+    component:NotFound //404页面写在最后面
   }
 ]
 
