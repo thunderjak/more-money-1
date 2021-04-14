38280161279015d53aa983153d8861f4fe6c64fa
thunderjak <1026253290@qq.com>
2021-04-15 00:17:42 +0800

    money的numberPad添加css

--- src/views/money.vue
+++ src/views/money.vue
@@ -1,10 +1,16 @@
 <template>
     <div>
         <Layout>
+            <div class="type">
+                <ul class="types">
+                    <li class="selected">支出</li>
+                    <li>收入</li>
+                </ul>
+            </div>
             <div class="tags">
                 <ul class="current">
                     <li>衣</li>
-                    <li>食</li>
+                    <li class="selected">食</li>
                     <li>住</li>
                     <li>行</li>
                 </ul>
@@ -18,27 +24,21 @@
                     <input type="text" placeholder="在这里输入备注">
                 </label>    
             </div>
-            <div class="type">
-                <ul class="types">
-                    <li class="selected">支出</li>
-                    <li>收入</li>
-                </ul>
-            </div>
-            <div class="numeberPad">
-                <div class="output">0</div>
+            <div class="numberPad">
+                <div class="output">100</div>
                 <div class="buttons">
                     <button>1</button>
                     <button>2</button>
                     <button>3</button>
-                    <button>date</button>
+                    <button class="date">date</button>
                     <button>4</button>
                     <button>5</button>
                     <button>6</button>
-                    <button>clear</button>
+                    <button class="delete">删除</button>
                     <button>7</button>
                     <button>8</button>
                     <button>9</button>
-                    <button>submit</button>
+                    <button class="submit">确认</button>
                     <button>00</button>
                     <button>0</button>
                     <button>.</button>
@@ -65,12 +65,16 @@
         display: flex;
         justify-content: center;
         align-items: center;
-          background: #d9d9d9;
-          $h:24px;
-          height: $h;
-          border-radius: $h/2;
-          padding: 0 $h/2;
-          margin-right: $h/2;
+        background: #d9d9d9;
+        $h:24px;
+        height: $h;
+        border-radius: $h/2;
+        padding: 0 $h/2;
+        margin-right: $h/2;
+        &.selected{
+            background: $color-highlight;
+            color: rgb(255, 255, 255);
+        }
       }
     }
     > .new{
@@ -90,7 +94,7 @@
     display: block;
     padding-left: 16px;
     display: flex;
-    align-items: center;
+    align-items: center;//水平居中
     .name{
         padding-right: 16px;
     }
@@ -100,6 +104,81 @@
         background: transparent;
         border: none;
         padding-right: 16px;
+    }
+}
+.type{
+    > .types{
+        background: #c4c4c4;
+        display: flex;
+        text-align: center;
+        font-size: 24px;
+        > li{
+            width: 50%;
+            height: 64px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            position: relative;
+            &.selected{
+                color: $color-highlight;
+            }
+            &.selected::after{
+                content: '';
+                position: absolute;
+                bottom:0;
+                left: 0;
+                width: 100%;
+                height: 4px;
+                background:$color-highlight;
+            }
+        }
+    }
+}
+.numberPad{
+    .output{
+        font-size: 36px;
+        font-family: Consolas,monospace;
+        padding: 9px 16px;
+        text-align: right;
+        box-shadow: inset 0 -5px 5px -5px fade-out(black,0.7),
+                    inset 0 5px 5px -5px fade-out(black,0.7)
+    }
+    .buttons{
+        //用float布局要写clearfix
+        @extend %clearFix;//写在helper里再引用
+        > button{
+            width: 25%;
+            height: 64px;
+            float: left;
+            background: transparent;
+            border: none;
+            &.submit{
+                height: 64*2px;
+                float: right;
+            }
+            $bg:#f2f2f2;
+            &:nth-child(1){
+                background: $bg;
+            }
+            &:nth-child(2),&:nth-child(5){
+                background: darken($bg, 4%);
+            }
+            &:nth-child(3),&:nth-child(6),&:nth-child(9){
+                background: darken($bg, 4*2%);
+            }
+            &:nth-child(4),&:nth-child(7),&:nth-child(10),&:nth-child(13){
+                background: darken($bg, 4*3%);
+            }
+            &:nth-child(8),&:nth-child(11),&:nth-child(14){
+                background: darken($bg, 4*4%);
+            }
+            &:nth-child(15){
+                background: darken($bg, 4*5%);
+            }
+            &:nth-child(12){
+                background: darken($bg, 4*6%);
+            }
+        }
     }
 }
 </style>
