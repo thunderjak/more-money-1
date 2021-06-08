96857dc0c9ef501173cfd4aa95b120627cfd49a0
thunderjak <1026253290@qq.com>
2021-06-09 00:46:37 +0800

    删除models

--- src/custon.d.ts
+++ src/custon.d.ts
@@ -10,12 +10,4 @@ type RecordItem = {
     id: string;
     name: string;
   }
-  type TagListModel = {
-    data: Tag[]
-    fetch: () => Tag[]
-    create: (name: string) => 'success' | 'duplicated' // 联合类型
-    update: (id: string, name: string) => 'success' | 'not found' | 'duplicated'
-    remove: (id: string) => boolean
-    save: () => void
-  }
   
